591824fe1c35508c366c44bc3cf7321548eeef12
Jeferson Chagas <jeferson_chagas25@hotmail.com>
2024-06-28 00:24:54 -0300

    feat: Implementando crud de brands.

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -2,41 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\BrandStoreRequest;
+use App\Http\Requests\BrandUpdateRequest;
 use App\Models\Brand;
+use App\Services\BrandServices;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
+
+    public function __construct(protected BrandServices $brandServices)
+    {
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        //
+        return response()->json($this->brandServices->list());
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(BrandStoreRequest $request): JsonResponse
     {
-        //
+        return response()->json($this->brandServices->store($request));
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Brand $brand)
+    public function show(Brand $brand): JsonResponse
     {
-        //
+        return response()->json($brand);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Brand $brand)
+    public function update(BrandUpdateRequest $request, Brand $brand): JsonResponse
     {
-        //
+        return response()->json($this->brandServices->update($request, $brand));
     }
 
     /**
@@ -44,6 +53,6 @@ class BrandController extends Controller
      */
     public function destroy(Brand $brand)
     {
-        //
+        return response()->json($this->brandServices->delete($brand));
     }
 }
